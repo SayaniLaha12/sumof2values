7f8e3e625d1b48bde0d98c62f27e95b70466fa21
Sayani Laha <sayanilaha129@gmail.com>
2023-07-19 22:51:35 +0530

    commiting try

---
+++ src/maxjava/iml/Main.java
@@ -0,0 +1,16 @@
+package maxjava.iml;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       //Variables
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b =sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+
+    }
+}
